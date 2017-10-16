617da980b901a145a00b15688dddebf36ae0df1a
Vatsal Shah <Moxit Shah>
2017-10-16 11:02:14 -0700

    Updated messages

--- profile.js
+++ profile.js
@@ -6,3 +6,29 @@ function addTags(){
     tagList.appendChild(entry);
     document.getElementById('tag-textbox').value = '';
 }
+ 
+    setTimeout(function(){
+var video = document.querySelector("#videoElement");
+ 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+if (navigator.getUserMedia) {       
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+ 
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+function videoError(e) {
+    // do something
+}
+},1000);
+
+function takePic(){
+        var video = document.getElementById("videoElement");
+        var canvas = document.getElementById("profile-image");
+        canvas.getContext("2d").drawImage(video,0,0,150,150);
+        var img = canvas.toDataURL("image/png");
+        video.style.display = "none";
+        canvas.style.display="inline";
+}
